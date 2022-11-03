b8bc53e88b18d9037d79649d46fde4ab2752722d
Web-projektitII <jukka.aula@contactorconnections.com>
2022-11-03 14:39:10 +0200

    mailtrap

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+    MAIL_SERVER = os.environ.get('MAILTRAP_MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAILTRAP_MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAILTRAP_MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAILTRAP_MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAILTRAP_MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flaskprojekti]'
     SP_MAIL_SENDER = 'Flaskprojekti Admin <flaskprojekti@example.com>'
     SP_ADMIN = os.environ.get('SP_ADMIN')
