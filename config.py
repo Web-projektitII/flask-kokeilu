780977c640afb42277da8c824fd396d9ca9c1dae
Web-projektitII <jukka.aula@contactorconnections.com>
2022-11-04 10:08:32 +0200

    uusi .gitignore

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     MAIL_SERVER = os.environ.get('MAILTRAP_MAIL_SERVER', 'smtp.googlemail.com')
